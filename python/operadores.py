3dff4e16109815c229957abe7ac9a802b5b2f993
name <email@mail.mail>
2017-03-10 16:58:47 +0100

    ---

---
+++ python/operadores.py
@@ -0,0 +1,38 @@
+numero1 = 10
+numero2 = 5
+#Sumar +
+total = numero1 + numero2
+print('La suma de',numero1,'y',numero2,'es igual a',total)
+#Resta -
+total = numero1 - numero2
+print('La diferencia entre',numero1,'y',numero2,'es igual a',total)
+#Multiplicación *
+total = numero1 * numero2
+print('La multiplicación de',numero1,'y',numero2,'es',total)
+#División /
+total = numero1 / numero2
+print('La división entre',numero1,'y',numero2,'es',total)
+#Orden de operaciones
+total = 3 + 3 * 3 #es igual a 12
+print('Primer resultado sin paréntesis:',total)
+total = (3+3) * 3 #es igual a 18
+print('Segundo resultado con paréntesis:',total)
+#Resto de una división %
+total = numero1 % numero2
+print('El resto de la división entre',numero1,'y',numero2,'es',total)
+#Cociente de una división //
+total = 10 // 3
+print('El cociente de',numero1,'entre',numero2,'es',total)
+#Potencias **
+total = numero1 ** numero2
+print(numero1,'elevado a',numero2,'es',total)
+#Asignación acumulada +=
+numero3 = numero1
+numero3 += 5
+numero3 += 2
+print('Número 3 es igual a',numero3)
+# -=
+numero3 = numero1
+numero3 -=5
+numero3 -=2
+print('Numero 3 ahora vale',numero3)

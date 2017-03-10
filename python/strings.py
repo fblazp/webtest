70f537945fe0f22cf6089e9096717bba05db5194
name <email@mail.mail>
2017-03-10 16:20:45 +0100

    ---

---
+++ python/strings.py
@@ -0,0 +1,20 @@
+#Strings
+#Incluir caracteres especiales en la cadena y que no generen conflicto \
+cadena = 'Bienvenidos a \"Python\"'
+print(cadena)
+#Extrae un caracter de la cadena
+saluda = 'Hola mundo'
+print(saluda[3])
+#Extraer una subcadena
+print(saluda[5:10])
+#Extraer subcadena desde la posicion index indicada hasta el final
+print(saluda[2:])
+#Multiplicar string
+print(saluda*3)
+#Concatenar Strings
+print("Hola"+"concatenado")
+#Concatenar Variables
+variable = 'Hola'
+variable += 'Mundooo'
+variable += 'Python'
+print(variable)

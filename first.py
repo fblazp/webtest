2dd5345bf5a18a9c8d09794415f4bff76860f033
name <email@mail.mail>
2017-03-10 15:31:53 +0100

    ---

---
+++ first.py
@@ -0,0 +1,26 @@
+print("Hola Mundo")
+#Variables
+string = "Hola Mundo String"
+print(string)
+#cambio de valor
+string = "Estoy cambiando de valor"
+print(string)
+
+a,b,c = 1, 2, 3
+print('Valor de las variables a, b y c',a, b, c) #concatenar
+#Eliminar variables con del
+del a, b, c
+#print(a, b, c)
+#tipos de datos y concatenar
+string = 'Cadena texto'
+integer = 12
+float = 3.1416
+#convertir tipo de datos en las variables
+#convertir a String str()
+concatenar = string +' '+str(integer)+' '+str(float)
+print(concatenar)
+#convertir un string a interger int()
+string ='1'
+integer = 1
+total = int(string) + integer
+print('El total es:', total)

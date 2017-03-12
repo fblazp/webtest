b30326194d6e6e3fde41f1631084ff40033ecc56
name <email@mail.mail>
2017-03-12 20:25:37 +0100

    ---

--- python/array-diccionario.py
+++ python/array-diccionario.py
@@ -1,1 +1,7 @@
-#diccionarios
+#diccionarios, se accede a ellos a través de una clave asignada
+diccionario = {'1': 'Python', '2': 'Java', 3: 'PHP','otro': 'C++'}
+#acceder al diccionario
+print(diccionario[3])
+print(diccionario['2'])
+print(diccionario['otro'])
+#mostrar claves del diccionario

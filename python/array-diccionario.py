85dd3f83fc278f4657be1b92be676a0f92488ef4
name <email@mail.mail>
2017-03-12 20:32:35 +0100

    ---

--- python/array-diccionario.py
+++ python/array-diccionario.py
@@ -4,4 +4,9 @@ diccionario = {'1': 'Python', '2': 'Java', 3: 'PHP','otro': 'C++'}
 print(diccionario[3])
 print(diccionario['2'])
 print(diccionario['otro'])
-#mostrar claves del diccionario
+#mostrar claves del diccionario .keys()
+print("Las claves del diccionario son:", diccionario.keys())
+#extraer los valores del diccionario .values()
+print("Los valores del diccionario son:", diccionario.values())
+#extraer el diccionario completo
+print("El diccionario completo es:", diccionario)

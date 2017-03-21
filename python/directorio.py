1941a4c1e45317ca3fa0790f329ce6d93aed4fd0
Yop <mi@enauk.net>
2017-03-21 19:04:11 +0100

    ---

---
+++ python/directorio.py
@@ -0,0 +1,5 @@
+#Módulos glob y os
+import glob, os.path
+#glob permite indexar en la ruta seleccionada para examinar los archivos y documentos que contiene
+
+#path es un submódulo de os, que nos permitirá entre otras cosas saber si un archivo o carperta existe en la ruta indicada

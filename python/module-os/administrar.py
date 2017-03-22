69498770d4535c855c7d4fe6af875049379cb9b4
mac <mac@windorr.mer>
2017-03-22 13:09:06 +0100

    ---

--- python/module-os/administrar.py
+++ python/module-os/administrar.py
@@ -6,18 +6,23 @@ def init():
         ruta = input('Indica la ruta, si no se indica será la actual: ')
         if(ruta == ''):
             ruta = os.getcwd() + '\\'
-            #Comprobar si la ruta existe
-            if(os.path.isdir(ruta)):
-                tipo = input('Indica el tipo a=archivo y c=carpeta: ')
-                if(tipo == 'a'):
-                    archivo = input('Indica el nombre del archivo: ')
-                    #Crear el archivo en la ruta indicada
-                    manejador = open(ruta+archivo, 'w')
-                    manejador.close()
-                    print('Archivo', archivo, 'creado con éxito')
-                elif(tipo == 'c'):
-                    carpeta = input('Indica el nombre de la carpeta')
-                    #Crear la carpeta
-                    os.mkdir(ruta+carpeta)
-                    print('Carpeta',carpeta, 'creada con éxito')
-                else: init() #Reinicia el programa
+        #Comprobar si la ruta existe
+        if(os.path.isdir(ruta)):
+            tipo = input('Indica el tipo a=archivo y c=carpeta: ')
+            if(tipo == 'a'):
+                archivo = input('Indica el nombre del archivo: ')
+                #Crear el archivo en la ruta indicada
+                manejador = open(ruta+archivo, 'w')
+                manejador.close()
+                print('Archivo', archivo, 'creado con éxito')
+            elif(tipo == 'c'):
+                carpeta = input('Indica el nombre de la carpeta')
+                #Crear la carpeta
+                os.mkdir(ruta+carpeta)
+                print('Carpeta',carpeta, 'creada con éxito')
+            else: init() #Reinicia el programa
+    elif(opcion == 'e'):
+        ruta = input('Indica la ruta, si no se indica será la actual: ')
+        if(ruta == ''):
+            ruta = os.getcwd() + '\\'
+        eliminar = input('Indica el nombre del archivo o carpeta a eliminar: ')
